b60ab4c410fcf8f2e3d5773295c4d42fd888321e
shrikrishnaborde <sbbordeshrikrishna@gmail.com>
2025-01-05 05:14:35 +0530

    wrong way to update state

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Counter from './components/Counter';
 import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
-      <Message />
+      {/*<Message /> */}
+
+      <Counter />
     </div>
   );
 }
---
+++ hello-world/src/components/Counter.js
@@ -0,0 +1,30 @@
+import { Component } from "react";
+
+class Counter extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
+
+  incrementCount(){
+    //wrong way
+    // this.state.count = this.state.count + 1;
+    
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={() => this.incrementCount()}>Increment</button>
+      </div>
+    )
+  }
+}
+
+export default Counter;
