14725977d5bb0f88edd957d38d7d198a7b37bb89
shrikrishnaborde <sbbordeshrikrishna@gmail.com>
2025-01-05 04:45:46 +0530

    state in class components

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,16 +1,10 @@
 import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Bruce" heroName="Batman">
-        <p>Children props</p>
-      </Greet>
-      <Welcome name="Clark" heroName="Superman">
-        <p>Class Childrn props</p>
-      </Welcome>
+      <Message />
     </div>
   );
 }
