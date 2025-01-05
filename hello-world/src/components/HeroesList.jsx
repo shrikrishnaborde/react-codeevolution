7d6179906a4ee5b2ee270eee9ab899a3d9cdf374
shrikrishnaborde <sbbordeshrikrishna@gmail.com>
2025-01-05 17:52:06 +0530

    key-prop

--- hello-world/src/components/HeroesList.jsx
+++ hello-world/src/components/HeroesList.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function HeroesList({heroes}) {
-  const heroesList = heroes.map(hero => <h2>{hero.name} a.k.a {hero.heroName}</h2>)
+  const heroesList = heroes.map(hero => <h2 key={hero.id}>{hero.name} a.k.a {hero.heroName}</h2>)
     return (
       <div>
       {heroesList}
--- hello-world/src/components/NamedList.js
+++ hello-world/src/components/NamedList.js
@@ -3,12 +3,15 @@ import HeroesList from './HeroesList';
 
 function NamedList() {
   const heroes = [{
+    id: 110057,
     name: 'Bruce',
     heroName: 'Batman'
   }, {
+    id: 157545,
     name: 'Clark', 
     heroName: 'Superman'
   }, {
+    id: 257545,
     name: 'Diana',
     heroName: 'Wonder Women'
   }];
