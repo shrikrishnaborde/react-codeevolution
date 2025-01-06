c5549e1fbd7a96ad3195f63f62c519f967bab456
shrikrishnaborde <sbbordeshrikrishna@gmail.com>
2025-01-06 11:27:44 +0530

    styling-components-3

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import InlineStyles from './components/InlineStyles';
 import StyleSheet from './components/StyleSheet';
 
 function App() {
   return (
     <div className="App">
-    <StyleSheet primary="true"/>
+    {/*<StyleSheet primary="true"/>*/}
+    <InlineStyles />
     </div>
   );
 }
---
+++ hello-world/src/components/InlineStyles.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function InlineStyles() {
+  const stylesObj = {
+    fontSize: 'large',
+    color: 'green'
+  }
+  return (
+    <div style={stylesObj}>InlineStyles</div>
+  )
+}
+
+export default InlineStyles
