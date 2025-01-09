ddecfa80e497c70bc7c51a910cd469d493a50068
shrikrishnaborde <sbbordeshrikrishna@gmail.com>
2025-01-09 07:06:30 +0530

    forward refs

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import RefsDemo from './components/RefsDemo';
 import FocusInput from './components/FocusInput';
+import FRInputParentRef from './components/FRInputParentRef';
 
 function App() {
   return (
     <div className="App">
       {/*<RefsDemo />*/}
-      <FocusInput />
+      {/*<FocusInput />*/}
+      <FRInputParentRef />
     </div>
   );
 }
