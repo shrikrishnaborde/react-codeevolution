db0efe64cfc569ba4f7c9f70e942be62d42b20b2
shrikrishnaborde <sbbordeshrikrishna@gmail.com>
2025-01-10 05:46:55 +0530

    error-boundry

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Hero from './component/Hero';
+import ErrorBoundry from './component/ErrorBoundry';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ErrorBoundry>
+          <Hero heroName="Batman"/>
+          <Hero heroName="Superman"/>
+          <Hero heroName="Joker"/>
+      </ErrorBoundry>
     </div>
   );
 }
