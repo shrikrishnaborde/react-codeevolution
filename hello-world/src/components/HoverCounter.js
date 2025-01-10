a29950c98b4a3e38596ccea25955292413e7742d
shrikrishnaborde <sbbordeshrikrishna@gmail.com>
2025-01-10 06:31:42 +0530

    higher order components

---
+++ hello-world/src/components/HoverCounter.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import withCounter from './withCounter';
+
+
+export class HoverCounter extends Component {
+  render() {
+    const {count, incrementCount} = this.props;
+    return (
+      <>
+      <h1 onMouseOver={incrementCount}>Hovered {count} times</h1>
+      </>
+    )
+  }
+}
+
+export default withCounter(HoverCounter,5)
